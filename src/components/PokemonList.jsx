23c0e7b0196ec0f3e48056aa7dae19aeaea91c19
nahuelcstr <nahuel.castro.0ne@gmail.com>
2025-10-17 01:43:36 -0300

    correccion de estilos varios

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -22,43 +22,45 @@ function PokemonList() {
 	};
 
 	return (
-		<div className="pokemon-container">
-			{list.map((poke) => (
-				<div key={poke.id} className="pokemon-card">
-					<h3 className="pokemon-card-text">
-						{poke.name} #{poke.id}
-					</h3>
-					<img
-						className="pokemon-image"
-						src={poke.sprites.other["official-artwork"].front_default}
-						alt={poke.name}
-					/>
-					<p>
-						<strong></strong>{" "}
-						{poke.types.map((t, i) => (
-							<span key={i} className={`btn-type-card ${t.type.name}`}>
-								{t.type.name}
+		<>
+			<div className="pokemon-container">
+				{list.map((poke) => (
+					<div key={poke.id} className="pokemon-card">
+						<h3 className="pokemon-card-text">
+							{poke.name} #{poke.id}
+						</h3>
+						<img
+							className="pokemon-image"
+							src={poke.sprites.other["official-artwork"].front_default}
+							alt={poke.name}
+						/>
+						<p>
+							<strong></strong>{" "}
+							{poke.types.map((t, i) => (
+								<span key={i} className={`btn-type-card ${t.type.name}`}>
+									{t.type.name}
+								</span>
+							))}
+						</p>
+						<p>
+							<strong>Peso:</strong> {poke.weight / 10} kg
+						</p>
+						<p>
+							<strong>Altura:</strong> {poke.height / 10} m
+						</p>
+						<p>
+							<strong>Habilidad:</strong>{" "}
+							<span className="pokemon-card-text">
+								{poke.abilities[0]?.ability.name}
 							</span>
-						))}
-					</p>
-					<p>
-						<strong>Peso:</strong> {poke.weight / 10} kg
-					</p>
-					<p>
-						<strong>Altura:</strong> {poke.height / 10} m
-					</p>
-					<p>
-						<strong>Habilidad:</strong>{" "}
-						<span className="pokemon-card-text">
-							{poke.abilities[0]?.ability.name}
-						</span>
-					</p>
-				</div>
-			))}
+						</p>
+					</div>
+				))}
+			</div>
 			<button className="btn-load" onClick={handleLoadMore}>
 				Cargar más
 			</button>
-		</div>
+		</>
 	);
 }
 
